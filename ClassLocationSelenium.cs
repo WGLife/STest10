c95420d531e15ec4f01f979237fe916246156ee1
Natalia Damorad <NataliaDamorad@coherentsolutions.com>
2018-03-15 17:40:50 +0300

    Added updated files

---
+++ ClassLocationSelenium.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTask1
+{
+    public class ClassLocationSelenium
+    {
+        public IWebDriver webDriver;
+        public ClassLocationSelenium()
+        {
+
+        }
+
+        public void FindWebElementByID(string id)
+        {
+
+            webDriver.FindElements(By.Id(id));
+        }
+
+        public void FindWebElementByName(string name)
+        {
+
+            webDriver.FindElement(By.Name(name));
+        }
+
+        public void FindWebElementByClassName(string className)
+        {
+
+            webDriver.FindElement(By.ClassName(className));
+        }
+
+        public void FindWeElementByCssSelector(string selector)
+        {
+            webDriver.FindElement(By.CssSelector(selector));
+        }
+
+        public void FindWebElemenByTagName(string tagName)
+        {
+
+            IReadOnlyList<IWebElement> elements = webDriver.FindElements(By.TagName(tagName));
+        }
+
+        public void FindWebElementByLinkText(string linkText)
+        {
+            webDriver.FindElement(By.LinkText(linkText));
+        }
+
+        public void FindWebElementByPartialLinkText(string partialLinkText)
+        {
+            webDriver.FindElement(By.PartialLinkText(partialLinkText));
+        }
+
+        public void FindWebElementByXPath(string xPath)
+        {
+            webDriver.FindElement(By.XPath(xPath));
+        }
+    }
+}
---
+++ Test1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace SeleniumTask1
+{
+    [TestClass]
+    public class Test1
+    {
+        IWebDriver driver = new ChromeDriver();
+        //FirefoxDriver driver = new FirefoxDriver();
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            driver.Navigate().GoToUrl("https://192.168.100.26/");
+            driver.FindElement(By.Id("Username")).SendKeys("EugenBorisik");
+            driver.FindElement(By.Id("Password")).SendKeys("qwerty12345");
+            driver.FindElement(By.Id("SubmitButton")).Click();
+            Console.WriteLine("URL: " + driver.Url.ToString());
+            Console.WriteLine("Title: " + driver.Title.ToString());
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(driver.Url.ToString().Contains("192.168.100.26") && (driver.Title.ToString() == "RMSys - Home"));
+        }
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            // 2. Create test 2, which perform logout from RMSys.
+            //driver.FindElementByLinkText("Sign Out").Click(); 
+            driver.Quit();
+        }
+    }
+}
