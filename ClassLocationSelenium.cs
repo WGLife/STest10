2036e8345e55528ef32149d68ea2c0e92307d67a
Eugen Borisik <eborisik@videologygroup.com>
2018-03-15 15:57:09 +0300

    Initial commit

---
+++ ClassLocationSelenium.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTask1
+{
+    public class ClassLocationSelenium
+    {
+        public ChromeDriver webDriver;
+        public ClassLocationSelenium()
+        {
+
+        }
+
+        public void FindWebElementByID(string id)
+        {
+            webDriver.FindElements(By.Id(id));
+            if (webDriver == null)
+            {
+                Console.WriteLine("The web element with ID: " + id + " was not found");
+            }
+        }
+        public void FindWebElementByName(string name)
+        {
+            webDriver.FindElement(By.Name(name));
+            if (webDriver == null)
+            {
+                Console.WriteLine("The web element with Name: " + name + " was not found");
+            }
+        }
+        public void FindWebElementByClassName(string className)
+        {
+            webDriver.FindElement(By.ClassName(className));
+            if (webDriver == null)
+            {
+                Console.WriteLine("The web element with Class Name: " + className + " was not found");
+            }
+        }
+        public void FindWebElemenByTagName(string tagName)
+        {
+            IReadOnlyList<IWebElement> elements = webDriver.FindElements(By.TagName(tagName));
+            if (elements == null)
+            {
+                Console.WriteLine("The web element with Tag Name: " + tagName + " was not found");
+            }
+        }
+        public void FindWebElementByLinkText(string linkText)
+        {
+            webDriver.FindElement(By.LinkText(linkText));
+            if (webDriver == null)
+            {
+                Console.WriteLine("The web element with Link Text: " + linkText + " was not found");
+            }
+        }
+        public void FindWebElementByPartialLinkText(string partialLinkText)
+        {
+            webDriver.FindElement(By.PartialLinkText(partialLinkText));
+            if (webDriver == null)
+            {
+                Console.WriteLine("The web element with partial Link Text: " + partialLinkText + " was not found");
+            }
+        }
+        public void FindWebElementByXPath(string xPath)
+        {
+            webDriver.FindElement(By.XPath(xPath));
+            if (webDriver == null)
+            {
+                Console.WriteLine("The web element by XPath: " + xPath + " was not found");
+            }
+        }
+    }
+}
---
+++ Test1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace SeleniumTask1
+{
+    [TestClass]
+    public class Test1
+    {
+        ChromeDriver driver = new ChromeDriver();
+        //FirefoxDriver driver = new FirefoxDriver();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl("https://192.168.100.26/");
+            driver.FindElementById("Username").SendKeys("EugenBorisik");
+            driver.FindElementById("Password").SendKeys("qwerty12345");
+            driver.FindElementById("SubmitButton").Click();
+            Console.WriteLine(driver.Url.ToString());
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(driver.Url.ToString().Contains("192.168.100.26"));
+        }
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            driver.FindElementByLinkText("Sign Out").Click();
+            driver.Quit();
+        }
+    }
+}
